ab6fa336d974cab3e08f1b5948fbc05cbfb02b98
virenKS <23637556+virenKS@users.noreply.github.com>
2023-08-06 17:36:22 +0530

    Add files via upload

---
+++ TARGET_NUCLEO_F446ZE/board_config.h
@@ -0,0 +1,114 @@
+#ifndef BOARDCONFIG_H
+#define BOARDCONFIG_H
+
+#include "pin.h"
+
+
+struct StepgenConfig {
+    const char* Comment;
+    const int JointNumber;
+    const char* StepPin;
+    const char* DirectionPin;
+  
+}; 
+
+struct EncoderConfig {
+    const char* Comment;
+    const char* PinA;
+    const char* PinB;
+    const int Modifier; // OPENDRAIN, PULLUP, PULLDOWN, PULLNONE, NONE
+
+
+};
+
+struct DigitalPinConfig {
+    const char* Comment;
+    const char* Pin;
+    const int Modifier; // OPENDRAIN, PULLUP, PULLDOWN, PULLNONE, NONE
+    const bool Invert;
+    const int DataBit;
+};
+
+struct PWMPinConfig {
+    const char* Comment;
+    const char* Pin;
+    const int Peroid_sp;
+
+};
+
+struct BlinkPinConfig {
+    const char* Comment;
+    const char* Pin;
+    const int Freq;
+};
+
+//Module config
+
+#define BOARD "NUCLEO446ze"
+
+//Base thread objects - Stepgens, encoders, and RC servos supported here
+//Comment, joint number, step pin, dir pin, 
+ 
+ //446
+StepgenConfig StepgenConfigs[] =   {{"X-Axis", 0, "PA_5", "PA_6"},
+                                    {"Z-Axis", 1, "PA_7", "PD_14"}};
+                                    
+/*
+
+StepgenConfig StepgenConfigs[] =   {{"X-Axis", 0, "PA_10", "PB_4"}, 
+                                    {"Y-Axis", 1, "PB_3", "PB_10"},
+                                    {"Z-Axis", 2, "PB_5", "PA_8"},
+                                    {"A-Axis", 3, "PA_6", "PA_5"}};
+StepgenConfig StepgenConfigs[] =   {{"X-Axis", 0, "PA_10", "PB_4","PA_9" }, 
+                                    {"Y-Axis", 1, "PB_3", "PB_10","PA_9" },
+                                    {"Z-Axis", 2, "PB_5", "PA_8","PA_9" },
+                                    {"A-Axis", 3, "PA_6", "PA_5","PA_9"  }};
+
+ 401
+StepgenConfig StepgenConfigs[] =   {{"X-Axis", 0, "PB_4", "PC_10","PA_9" }, 
+                                    {"Y-Axis", 1, "PB_10", "PB_12","PA_9" },
+                                    {"Z-Axis", 2, "PA_8", "PD_13","PA_9" },
+                                    {"A-Axis", 3, "PA_6", "PA_5","PA_9" }};
+*/
+
+
+//EncoderConfig EncoderConfigs[] = {{"X-axis", 0, "PA_13", "PA_14", PULLNONE}};
+EncoderConfig EncoderConfigs[] = {};
+//EncoderConfig EncoderConfigs[] = {};
+
+//Servo thread objects - eStop, Reset Pin, Blink, Digital Pin, PWM, Temperature, Switch, QEI
+
+DigitalPinConfig DOConfigs[] = {{"STEP_ENA", "PC_7", PULLNONE, false, 0}, //Comment, pin, modifier, invert, data bit
+                                {"COOLANT", "PB_5", PULLNONE, false, 1},
+                                {"AUX1", "PB_3", PULLNONE, false, 2}};
+
+DigitalPinConfig DIConfigs[] = {{"ALL_HOME", "PE_9", PULLUP, false, 0},
+                                {"RUN", "PE_11", PULLUP, false, 1},
+                                {"RUN", "PF_14", PULLUP, false, 2},
+                                {"Z_LIMIT", "PE_13", PULLUP, false, 2},
+                                {"ESTOP", "PF_15", PULLUP, false, 3},
+                                {"ABORT", "PG_14", PULLUP, false, 4},
+                                {"HOLD", "PG_9", PULLUP, false, 5}};  //Comment, pin, modifier, invert, data bit
+/*
+                                {"ChA", "PD_0", PULLUP, false, 6},
+                                {"Ch_Index", "PD_1", PULLUP, false, 7}};  //Comment, pin, modifier, invert, data bit
+
+*/
+
+
+//PWMPinConfig PWMConfigs[] = {};        
+PWMPinConfig PWMConfigs[] = {{"Spindle PWM", "PB_11"}};
+
+BlinkPinConfig BlinkConfigs[] = {};    
+
+
+const char* PRU_Reset_Pin = "PB_2";
+
+
+ 
+
+//On Load objects - MCP4451, Motor Power, TMC2208, TMC2209. None currently implemented here.
+
+
+
+#endif
